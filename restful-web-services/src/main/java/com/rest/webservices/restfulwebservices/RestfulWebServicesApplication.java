ee31aaa278d8b664d2aff9d9954bd992e2515979
Adam Tuck <adam.tuck@accenture.com>
2022-11-02 12:13:05 +0000

    test

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,7 +3,7 @@ package com.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplicatio
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
